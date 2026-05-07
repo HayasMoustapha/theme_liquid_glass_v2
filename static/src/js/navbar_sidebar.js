06e51ee1ac8646e743c28e9e25679dc824e5709b
HayasMoustapha <you@example.com>
2026-05-07 08:15:57 +0100

    l3 harden bao launcher runtime

--- static/src/js/navbar_sidebar.js
+++ static/src/js/navbar_sidebar.js
@@ -2,21 +2,33 @@
 
 import { NavBar } from "@web/webclient/navbar/navbar";
 import { patch } from "@web/core/utils/patch";
-import { useState } from "@odoo/owl";
+import { useEffect, useState } from "@odoo/owl";
 
 patch(NavBar.prototype, {
     setup() {
         super.setup();
         this.sidebarState = useState({ isOpen: false });
+        useEffect(
+            (isOpen) => {
+                document.body.classList.toggle("o_sidebar_lock", isOpen);
+                return () => {
+                    document.body.classList.remove("o_sidebar_lock");
+                };
+            },
+            () => [this.sidebarState.isOpen]
+        );
     },
+
     toggleSidebar() {
         this.sidebarState.isOpen = !this.sidebarState.isOpen;
     },
+
     closeSidebar() {
         this.sidebarState.isOpen = false;
     },
+
     onNavBarAppClick(app) {
         this.closeSidebar();
         this.menuService.selectMenu(app);
-    }
+    },
 });
